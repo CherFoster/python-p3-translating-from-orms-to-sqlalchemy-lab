9a3f00bc3af1bad12d2fdc8d49e3a6f2f14a503f
Cherise Foster <cherise.foster@outlook.com>
2023-06-27 13:12:42 -0600

    passed tests

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import (Column, String, Integer)
+from sqlalchemy import (create_engine, Index, Column, String, Integer)
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -8,6 +8,13 @@ Base = declarative_base()
 class Dog(Base):
     __tablename__ = 'dogs'
 
+    Index('index_name', 'name')
+
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     breed = Column(String())
+
+    def __repr__(self):
+        return f"Dog {self.id}: " \
+            + f"{self.name}, " \
+            + f"Breed: {self.breed}"
